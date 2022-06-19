14c36ffd1ea87ee95a65f22abc96d46919391af1
vitor <vitorg_98@outlook.com>
2022-06-18 23:20:52 -0300

    add-sql

---
+++ sql/script.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO TarefaPrioridade
+VALUES 
+('Baixa'),
+('Media'),
+('Alta')
+
+INSERT INTO TarefasStatus
+VALUES 
+('Backlog'),
+('ToDo'),
+('InProgress'),
+('Done')
+
+select * from Tarefa
+select * from TarefaPrioridade
+select * from TarefasStatus
+
